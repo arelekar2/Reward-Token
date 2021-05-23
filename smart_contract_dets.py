b17de2337dc966746182b4436c62d1244ceca4b9
arelekar2 <relekar.akhil@gmail.com>
2021-05-23 19:26:52 +0800

    add instruction to replace smart contract addr

--- smart_contract_dets.py
+++ smart_contract_dets.py
@@ -3,4 +3,5 @@ import json
 with open('reward_contract_abi.json') as f:
     contractABI = json.load(f)
 
+# replace below value with the actual smart contract address
 contractAddress = '0x17131B3b0766d482366dE553624C1bBF39e31805'
