e6c7b073335d62f97d32b6ac03700b95218569a8
arelekar2 <relekar.akhil@gmail.com>
2021-08-18 23:28:58 +0800

    add py script to run the Staking features

---
+++ StakingInteraction.py
@@ -0,0 +1,47 @@
+from web3 import Web3
+from smart_contract_dets import contractABI, contractAddress
+
+web3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+
+# set default acc to owner 
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+print(f'Connected to blockchain: {web3.isConnected()}')
+
+contract = web3.eth.contract(
+    address=web3.toChecksumAddress(contractAddress), 
+    abi=contractABI)
+print(f'Current Owner: {contract.functions.getOwner().call()}')
+
+
+# ================= Stake creation
+client1 = web3.eth.accounts[1]
+
+tx_hash = contract.functions.registerParticipant(client1, 1).transact()
+web3.eth.waitForTransactionReceipt(tx_hash)
+
+tx_hash = contract.functions.issueTokens(client1, 200).transact()
+web3.eth.waitForTransactionReceipt(tx_hash)
+
+tx_hash = contract.functions.createStake(100).transact()
+web3.eth.waitForTransactionReceipt(tx_hash)
+
+
+# ================= Stake & Reward check
+print(f'client1 stake: {contract.functions.stakeOf(client1).call()}')
+print(f'Total stakes in blockchain: {contract.functions.getTotalStakes().call()}')
+
+contract.functions.distributeStakingRewards().call()
+
+print(f'client1 reward: {contract.functions.stakingRewardOf(client1).call()}')
+print(f'Total distributed staking rewards: {contract.functions.getTotalStakingRewards().call()}')
+
+
+# ================= UnStaking & withdrawal
+tx_hash = contract.functions.removeStake(100).transact()
+web3.eth.waitForTransactionReceipt(tx_hash)
+
+tx_hash = contract.functions.withdrawStakingRewards().transact()
+web3.eth.waitForTransactionReceipt(tx_hash)
+
+print(f'Updated client1 Balance: {contract.functions.getBalanceFor(client1).call()}')
