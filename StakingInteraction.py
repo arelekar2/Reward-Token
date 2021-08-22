3c972619afa580f2c1250b893993b3b7531b6762
arelekar2 <relekar.akhil@gmail.com>
2021-08-22 23:57:16 +0800

    clean up staking interaction

--- StakingInteraction.py
+++ StakingInteraction.py
@@ -6,52 +6,42 @@ web3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
 # set default acc to owner 
 web3.eth.defaultAccount = web3.eth.accounts[0]
 
-print(f'===== Connected to blockchain: {web3.isConnected()} =====')
+print(f'\n===== Connected to blockchain: {web3.isConnected()} =====')
 
 contract = web3.eth.contract(
     address=web3.toChecksumAddress(contractAddress), 
     abi=contractABI)
-print(f'Current Owner: {contract.functions.getOwner().call()}')
 
 
-# ================= Stake creation
+# Participants
+owner = web3.eth.accounts[0]
 client1 = web3.eth.accounts[1]
 
-tx_hash = contract.functions.registerParticipant(client1, 1).transact()
-web3.eth.waitForTransactionReceipt(tx_hash)
-
-tx_hash = contract.functions.issueTokens(client1, 200).transact()
-web3.eth.waitForTransactionReceipt(tx_hash)
-
+# ================= Stake creation
 web3.eth.defaultAccount = client1
+print(f'client1 Balance (Before): {contract.functions.balanceOf(client1).call()}')
 tx_hash = contract.functions.createStake(10).transact()
 web3.eth.waitForTransactionReceipt(tx_hash)
+print(f'client1 stake: {contract.functions.stakeOf(client1).call()}')
 
 
 # ================= Stake & Reward check
-print(f'client1 stake: {contract.functions.stakeOf(client1).call()}\n')
-
-
-web3.eth.defaultAccount = web3.eth.accounts[0]
-print(f'Total stakes in blockchain: {contract.functions.getTotalStakes().call()}')
+web3.eth.defaultAccount = owner
+print(f'Total stakes in blockchain: {contract.functions.getTotalStakes().call()}\n')
 
 tx_hash = contract.functions.distributeStakingRewards().transact()
 web3.eth.waitForTransactionReceipt(tx_hash)
 
+print(f'client1 reward: {contract.functions.stakingRewardOf(client1).call()}')
 print(f'Total distributed staking rewards: {contract.functions.getTotalStakingRewards().call()}\n')
 
 
+# =================  Withdrawal & UnStaking 
 web3.eth.defaultAccount = client1
-print(f'client1 reward: {contract.functions.stakingRewardOf(client1).call()}')
-
-
-# ================= UnStaking & withdrawal
 tx_hash = contract.functions.withdrawStakingRewards().transact()
 web3.eth.waitForTransactionReceipt(tx_hash)
 
 tx_hash = contract.functions.removeStake(10).transact()
 web3.eth.waitForTransactionReceipt(tx_hash)
-
-
-print(f'Updated client1 Balance: {contract.functions.getSelfBalance().call()}')
-print('===== =====')
+print(f'client1 Balance (After): {contract.functions.getSelfBalance().call()}')
+print('===== =====\n')
